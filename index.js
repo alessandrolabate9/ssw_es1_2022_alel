c71a702f03cccd5dd602ac8662fe84cd98ffde02
alessandrolabate9 <alessandrolabate96@hotmail.it>
2022-03-04 15:19:50 +0100

    2 files modified

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import './style.css';
 
 // Write Javascript code!
 var btn = document.createElement("button");
+var valore = "Marianna"
 btn.innerHTML = "P";
 var posto = document.getElementById("posto");
 posto.appendChild(btn);
+btn.addEventListener("click", () => {
+  document.getElementById("nome").innerHTML = valore
+});
