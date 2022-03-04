71f29b840d8d2c8d9af9daa17db2f16bd726f015
alessandrolabate9 <alessandrolabate96@hotmail.it>
2022-03-04 15:21:36 +0100

    1 file modified

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 import './style.css';
 
 // Write Javascript code!
-var btn = document.createElement("button");
-var valore = "Marianna"
+const btn = document.createElement("button");
+const valore = "Marianna"
 btn.innerHTML = "P";
-var posto = document.getElementById("posto");
+const posto = document.getElementById("posto");
 posto.appendChild(btn);
 btn.addEventListener("click", () => {
   document.getElementById("nome").innerHTML = valore
