936af29e16c6a4b836be1fb6c63f59c76ede743c
alessandrolabate9 <alessandrolabate96@hotmail.it>
2022-03-05 11:50:07 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const btn = document.createElement('button');
-const valore = 'Marianna';
-var posto = document.getElementById('posto');
-//per ogni posizione creare un button
-// associamo un value vuoto
-// in alcuni value pieno
-const posizione = new Array(10);
-for(let i = 0; i<posizione.length; i++){
-  const btn00 = document.createElement("button");
+var posizioni = new Array(10);
+//prima posizione - esercizio precedente
+const posto = document.getElementById("posto");
+const primo_posto = document.createElement("button");
+posto.appendChild(primo_posto);
+primo_posto.innerHTML = "P0";
+primo_posto.setAttribute('class', 'posto');
+primo_posto.value = "Giovanni";
+//assegnamento nuove posizioni da oggetto-array 
+for (let i =1; i<posizioni.length; i++){
+  let btn00 = document.createElement("button");
+  btn00.setAttribute("id", "P"+i);
+  btn00.setAttribute('class', 'posto');
   btn00.innerHTML = "P"+i;
   posto.appendChild(btn00);
-};
-btn.addEventListener('click', () => {
-  document.getElementById('nome').innerHTML = valore;
-  
-});
+}
+//assegnamento nuovi valori
+document.getElementById("P3").value = "Marianna";
+document.getElementById("P6").value = "Giacomo";
+//gestione click button
+const elementi = Array.from(document.getElementsByClassName('posto'));
+for(let el of elementi){
+  el.click = () => {console.log("Pr");document.getElementById("nome").innerHTML = el.value};
+}
