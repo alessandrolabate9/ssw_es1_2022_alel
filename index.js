c4326f860cc5930df9027c95fcf36f7d302fd3e0
alessandrolabate9 <alessandrolabate96@hotmail.it>
2022-03-06 15:25:00 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 // Import stylesheets
 import './style.css';
+// due array con la stessa dimensione , posti e prenotazione
 
-var posizioni = new Array(10);
-//prima posizione - esercizio precedente
-const posto = document.getElementById("posto");
-const primo_posto = document.createElement("button");
-posto.appendChild(primo_posto);
-primo_posto.innerHTML = "P0";
-primo_posto.setAttribute('class', 'posto');
-primo_posto.value = "Giovanni";
-//assegnamento nuove posizioni da oggetto-array 
-for (let i =1; i<posizioni.length; i++){
-  let btn00 = document.createElement("button");
-  btn00.setAttribute("id", "P"+i);
-  btn00.setAttribute('class', 'posto');
-  btn00.innerHTML = "P"+i;
-  posto.appendChild(btn00);
-}
+var posti = new Array(10);
+var prenotazione = new Array(10);
+posti.fill("x");
+posti.forEach((this)=>{console.log(this)});
+
+
 //assegnamento nuovi valori
-document.getElementById("P3").value = "Marianna";
-document.getElementById("P6").value = "Giacomo";
-//gestione click button
-const elementi = Array.from(document.getElementsByClassName('posto'));
-for(let el of elementi){
-  el.addEventListener('click', () => {document.getElementById("nome").innerHTML = el.value});
-}
