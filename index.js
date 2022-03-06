e085c61abd3a408616c26635a33b2f1b750d815c
alessandrolabate9 <alessandrolabate96@hotmail.it>
2022-03-06 16:13:52 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // Import stylesheets
 import './style.css';
-// due array con la stessa dimensione , posti e prenotazione
 
+const posto = document.getElementById('posto');
+const nome = document.getElementById('nome');
+// due array con la stessa dimensione , posti e prenotazione
 var posti = new Array(10);
-var prenotazione = new Array(10);
+var prenotazioni = new Array(10);
+
 posti.fill("x");
-posti.forEach((this)=>{console.log(this)});
+posti[0] = "Giovanni";
+posti[3] = "Marianna";
+posti[6] = "Giacomo";
+// posti.forEach((posto, index)=>{console.log(posto, index);});
+posti.map((x, index) => {
+  let btn = document.createElement('button');
+  btn.innerHTML = "P"+index;
+  btn.value = x;
+  posto.appendChild(btn);
+  btn.onclick = () => nome.innerHTML = btn.value;
+});
+
 
 
 //assegnamento nuovi valori
