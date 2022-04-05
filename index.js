5490a69841915e3e051e2bd45450eb4be6bbc5fd
alessandrolabate9 <alessandrolabate96@hotmail.it>
2022-04-05 16:59:13 +0200

    1 file modified

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const nome = document.getElementById('nome');
 const posti = 10;
 const file = 5;
 const prenotazione = new Array(file).fill("").map(()=>Array(posti).fill('x'));
+const prenotazione_palchi ="";
 prenotazione[0][7] = "Aldo";
 prenotazione[1][3] = "Giovanna";
 prenotazione[4][2] = "Giacomo";
