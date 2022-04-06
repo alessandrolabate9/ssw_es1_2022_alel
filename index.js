5f8ff3bc44cd575e88b04872ee6dc98ce35864f1
alessandrolabate9 <alessandrolabate96@hotmail.it>
2022-04-06 17:36:46 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Import stylesheets
 import './style.css';
 
-const posto = document.getElementById('posto');
+const platea = document.getElementById('platea');
 const nome = document.getElementById('nome');
+const palchi = document.getElementById('palchi');
 const posti = 10;
 const file = 5;
 const prenotazione = new Array(file).fill("").map(()=>Array(posti).fill('x'));
@@ -14,7 +15,7 @@ prenotazione[1][3] = "Giovanna";
 prenotazione[4][2] = "Giacomo";
 console.log(prenotazione);
 prenotazione.map((p, pindex) =>{
-  posto.appendChild(document.createElement("br"));
+  platea.appendChild(document.createElement("br"));
   p.map((f, findex)=>{
     const btn = document.createElement('button');
     btn.value = f;
@@ -24,6 +25,6 @@ prenotazione.map((p, pindex) =>{
     else
       btn.style="color:red";
     btn.onclick = () => nome.innerHTML = btn.value;
-    posto.appendChild(btn);
+    platea.appendChild(btn);
   });
 })
